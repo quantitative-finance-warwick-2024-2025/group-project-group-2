0aa0ba087cf5ca354f14e7508d2778ff4dcc301c
NeophyteKDB17 <130423878+NeophyteKDB17@users.noreply.github.com>
2025-03-12 11:06:41 +0000

    1

--- src/LookbackConvergence.cpp
+++ src/LookbackConvergence.cpp
@@ -39,8 +39,8 @@ int main() {
     std::cout << "periods, Call Naive, Call Antithetic, Call Strat_Sampling, Call Control Variates, Put Naive, Put Antithetic, Put Strat_Sampling, Put Control Variates\n";
     unsigned int nSimConvergence = 1000;
     for (unsigned int p : {1<<2, 1<<3, 1<<4, 1<<5, 1<<6, 1<<7, 1<<8, 1<<9, 1<<10, 1<<11}) {
-        LookbackOption lbCall(K, T, Option::Call, p, LookbackOption::Floating);
-        LookbackOption lbPut(K, T, Option::Put, p, LookbackOption::Floating);
+        LookbackOption lbCall(K, T, Option::Call, p, LookbackOption::Fixed);
+        LookbackOption lbPut(K, T, Option::Put, p, LookbackOption::Fixed);
     
         double naivePriceC = PriceClass::calculateP_Naive(lbCall, S, r, sigma, T, nSimConvergence);
         double antiPriceC  = PriceClass::calculateP_Antithetic(lbCall, S, r, sigma, T, nSimConvergence);
